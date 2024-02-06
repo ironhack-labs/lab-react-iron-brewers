93def92ae65fb6f8be519c13f66473d045df78ca
VladysslavTkach <tkachacc.program@gmail.com>
2024-02-06 15:37:31 +0100

    solved lab

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,23 @@
-function Search() {
-  return (
-    <div className="d-inline-flex justify-content-center align-items-center w-100 p-4">
-      <div className="input-group mb-2 w-50">
-        <div className="input-group-prepend">
-          <span className="input-group-text" id="basic-addon1">
-            Search
-          </span>
-        </div>
-        <input
-          type="text"
-          className="form-control search-bar"
-        />
-      </div>
-    </div>
-  );
+import { useState } from 'react';
+import axios from 'axios';
+
+function Search({ handlerFilter }) {
+	return (
+		<div className="d-inline-flex justify-content-center align-items-center w-100 p-4">
+			<div className="input-group mb-2 w-50">
+				<div className="input-group-prepend">
+					<span className="input-group-text" id="basic-addon1">
+						Search
+					</span>
+				</div>
+				<input
+					onChange={handlerFilter}
+					type="text"
+					className="form-control search-bar"
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default Search;
