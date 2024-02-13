35ecb742a5c7e4a3814c2dcdc4b7da1f38240862
Fran Pazos <franciscopazosrey@gmail.com>
2024-02-13 16:08:34 +0100

    lab done

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,27 @@
-function Search() {
+import { useState } from "react";
+
+function Search({ searchHandler }) {
+  const [string, setString] = useState("");
+
+  const handleSearch = (e) => {
+    setString(e.target.value);
+    searchHandler(e.target.value);
+  };
+
   return (
-    <div className="d-inline-flex justify-content-center align-items-center w-100 p-4">
-      <div className="input-group mb-2 w-50">
-        <div className="input-group-prepend">
-          <span className="input-group-text" id="basic-addon1">
-            Search
-          </span>
-        </div>
+    <>
+      <div
+        style={{ display: "flex", justifyContent: "center", margin: "15px" }}
+      >
+        <label>Search for Beers</label>
         <input
           type="text"
-          className="form-control search-bar"
+          className="search-bar"
+          value={string}
+          onChange={handleSearch}
         />
       </div>
-    </div>
+    </>
   );
 }
 
