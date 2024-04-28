1f73a51ef03d251360136264b402b2de267759c6
tanyakanf <tanyakanf@gmail.com>
2024-04-28 18:57:10 +0200

    done

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,13 @@
-function Search() {
+import { useState } from "react";
+
+function Search({ searchHandler }) {
+  const [string, setString] = useState("");
+
+  const handleSearch = (e) => {
+    setString(e.target.value);
+    searchHandler(e.target.value);
+  };
+
   return (
     <div className="d-inline-flex justify-content-center align-items-center w-100 p-4">
       <div className="input-group mb-2 w-50">
@@ -10,6 +19,8 @@ function Search() {
         <input
           type="text"
           className="form-control search-bar"
+          value={string}
+          onChange={handleSearch}
         />
       </div>
     </div>
