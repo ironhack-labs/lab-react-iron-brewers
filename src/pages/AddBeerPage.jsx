fc5ccf038ff65e37e73e292f4dc2b639b5203a72
Shakthi Prasad KT <shakthi.kt@gmail.com>
2024-05-31 14:58:53 +0200

    done

--- src/pages/AddBeerPage.jsx
+++ src/pages/AddBeerPage.jsx
@@ -1,131 +1,155 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const API_URL = "https://ih-beers-api2.herokuapp.com/beers";
 
 function AddBeerPage() {
-  // State variables to store the values of the form inputs. You can leave these as they are.
-  const [name, setName] = useState("");
-  const [tagline, setTagline] = useState("");
-  const [description, setDescription] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
-  const [firstBrewed, setFirstBrewed] = useState("");
-  const [brewersTips, setBrewersTips] = useState("");
-  const [attenuationLevel, setAttenuationLevel] = useState(0);
-  const [contributedBy, setContributedBy] = useState("");
+  // State variables to store the values of the form inputs.
+  const [formData, setFormData] = useState({
+    name: "",
+    tagline: "",
+    description: "",
+    imageUrl: "",
+    firstBrewed: "",
+    brewersTips: "",
+    attenuationLevel: 0,
+    contributedBy: "",
+  });
 
-  // Handler functions for the form inputs. You can leave these as they are.
-  const handleName = (e) => setName(e.target.value);
-  const handleTagline = (e) => setTagline(e.target.value);
-  const handleDescription = (e) => setDescription(e.target.value);
-  const handleImageUrl = (e) => setImageUrl(e.target.value);
-  const handleFirstBrewed = (e) => setFirstBrewed(e.target.value);
-  const handleBrewersTips = (e) => setBrewersTips(e.target.value);
-  const handleAttenuationLevel = (e) => setAttenuationLevel(e.target.value);
-  const handleContributedBy = (e) => setContributedBy(e.target.value);
+  const navigate = useNavigate();
 
+  // Handler functions for the form inputs.
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
+  // Function to handle the form submission
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
 
-  // TASK:
-  // 1. Create a function to handle the form submission and send the form data to the Beers API to create a new beer.
-  // 2. Use axios to make a POST request to the Beers API.
-  // 3. Once the beer is created, navigate the user to the page showing the list of all beers.
+    const newBeer = {
+      name: formData.name,
+      tagline: formData.tagline,
+      description: formData.description,
+      image_url: formData.imageUrl,
+      first_brewed: formData.firstBrewed,
+      brewers_tips: formData.brewersTips,
+      attenuation_level: formData.attenuationLevel,
+      contributed_by: formData.contributedBy,
+    };
 
+    axios
+      .post(`${API_URL}/new`, newBeer)
+      .then((response) => {
+        console.log("response", response);
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
 
-
-  // Structure and the content of the page showing the form for adding a new beer. You can leave this as it is.
+  // Structure and content of the page showing the form for adding a new beer.
   return (
-    <>
-      <div className="d-inline-flex flex-column w-100 p-4">
-        <form>
-          <label>Name</label>
-          <input
-            className="form-control mb-4"
-            type="text"
-            name="name"
-            placeholder="Beer Name"
-            value={name}
-            onChange={handleName}
-          />
-          <label>Tagline</label>
-          <input
-            className="form-control mb-4"
-            type="text"
-            name="tagline"
-            placeholder="Beer Tagline"
-            value={tagline}
-            onChange={handleTagline}
-          />
+    <div className="d-inline-flex flex-column w-100 p-4">
+      <form onSubmit={handleFormSubmit}>
+        <label>Name</label>
+        <input
+          className="form-control mb-4"
+          type="text"
+          name="name"
+          placeholder="Beer Name"
+          value={formData.name}
+          onChange={handleInputChange}
+        />
 
-          <label className="form-label">Description</label>
-          <textarea
-            className="form-control mb-4"
-            type="text"
-            name="description"
-            placeholder="Description"
-            rows="3"
-            value={description}
-            onChange={handleDescription}
-          ></textarea>
+        <label>Tagline</label>
+        <input
+          className="form-control mb-4"
+          type="text"
+          name="tagline"
+          placeholder="Beer Tagline"
+          value={formData.tagline}
+          onChange={handleInputChange}
+        />
 
-          <label>Image</label>
-          <input
-            className="form-control mb-4"
-            type="text"
-            name="imageUrl"
-            placeholder="Image URL"
-            value={imageUrl}
-            onChange={handleImageUrl}
-          />
+        <label className="form-label">Description</label>
+        <textarea
+          className="form-control mb-4"
+          type="text"
+          name="description"
+          placeholder="Description"
+          rows="3"
+          value={formData.description}
+          onChange={handleInputChange}
+        ></textarea>
 
-          <label>First Brewed</label>
-          <input
-            className="form-control mb-4"
-            type="text"
-            name="firstBrewed"
-            placeholder="Date - MM/YYYY"
-            value={firstBrewed}
-            onChange={handleFirstBrewed}
-          />
+        <label>Image</label>
+        <input
+          className="form-control mb-4"
+          type="text"
+          name="imageUrl"
+          placeholder="Image URL"
+          value={formData.imageUrl}
+          onChange={handleInputChange}
+        />
 
-          <label>Brewer Tips</label>
-          <input
-            className="form-control mb-4"
-            type="text"
-            name="brewersTips"
-            placeholder="..."
-            value={brewersTips}
-            onChange={handleBrewersTips}
-          />
+        <label>First Brewed</label>
+        <input
+          className="form-control mb-4"
+          type="text"
+          name="firstBrewed"
+          placeholder="Date - MM/YYYY"
+          value={formData.firstBrewed}
+          onChange={handleInputChange}
+        />
 
-          <label>Attenuation Level</label>
-          <div className="input-group mb-2">
-            <div className="input-group-prepend">
-              <span className="input-group-text" id="basic-addon1">
-                %
-              </span>
-            </div>
-            <input
-              className="form-control mb-4"
-              type="number"
-              name="attenuationLevel"
-              value={attenuationLevel}
-              onChange={handleAttenuationLevel}
-              min={0}
-              max={100}
-            />
+        <label>Brewer Tips</label>
+        <input
+          className="form-control mb-4"
+          type="text"
+          name="brewersTips"
+          placeholder="..."
+          value={formData.brewersTips}
+          onChange={handleInputChange}
+        />
+
+        <label>Attenuation Level</label>
+        <div className="input-group mb-2">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="basic-addon1">
+              %
+            </span>
           </div>
-
-          <label>Contributed By</label>
           <input
             className="form-control mb-4"
-            type="text"
-            name="contributedBy"
-            placeholder="Contributed by"
-            value={contributedBy}
-            onChange={handleContributedBy}
+            type="number"
+            name="attenuationLevel"
+            value={formData.attenuationLevel}
+            onChange={handleInputChange}
+            min={0}
+            max={100}
           />
-          <button className="btn btn-primary btn-round">Add Beer</button>
-        </form>
-      </div>
-    </>
+        </div>
+
+        <label>Contributed By</label>
+        <input
+          className="form-control mb-4"
+          type="text"
+          name="contributedBy"
+          placeholder="Contributed by"
+          value={formData.contributedBy}
+          onChange={handleInputChange}
+        />
+
+        <button className="btn btn-primary btn-round">Add Beer</button>
+      </form>
+    </div>
   );
 }
 
