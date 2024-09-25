113c7987d6125214370b841fe43a1c3039b1274e
enriquepaez <enrique.paezpozo@gmail.com>
2024-09-25 18:04:56 +0200

    No bonus

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,21 +1,35 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Search from "../components/Search";
 import beersJSON from "./../assets/beers.json";
+import axios from "axios";
 
 
 
 function AllBeersPage() {
   // Mock initial state, to be replaced by data from the API. Once you retrieve the list of beers from the Beers API store it in this state variable.
-  const [beers, setBeers] = useState(beersJSON);
-
-
+  const [beers, setBeers] = useState(null);
 
   // TASKS:
   // 1. Set up an effect hook to make a request to the Beers API and get a list with all the beers.
-  // 2. Use axios to make a HTTP request.
-  // 3. Use the response data from the Beers API to update the state variable.
+  useEffect(() => {
+    getData()
+  }, [])
 
+  const getData = async () => {
+
+    try {
+        // 2. Use axios to make a HTTP request.
+      const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/beers`)
+      console.log(response)
+
+      // 3. Use the response data from the Beers API to update the state variable.
+      setBeers(response.data)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
 
   // The logic and the structure for the page showing the list of beers. You can leave this as it is for now.
@@ -24,32 +38,32 @@ function AllBeersPage() {
       <Search />
 
       <div className="d-inline-flex flex-wrap justify-content-center align-items-center w-100 p-4">
-        {beers &&
-          beers.map((beer, i) => {
-            return (
-              <div key={i}>
-                <Link to={"/beers/" + beer._id}>
-                  <div className="card m-2 p-2 text-center" style={{ width: "24rem", height: "18rem" }}>
-                    <div className="card-body">
-                      <img
-                        src={beer.image_url}
-                        style={{ height: "6rem" }}
-                        alt={"image of" + beer.name}
-                      />
-                      <h5 className="card-title text-truncate mt-2">{beer.name}</h5>
-                      <h6 className="card-subtitle mb-3 text-muted">
-                        <em>{beer.tagline}</em>
-                      </h6>
-                      <p className="card-text">
-                        Created by: {beer.contributed_by}
-                      </p>
+          {beers &&
+            beers.map((beer, i) => {
+              return (
+                <div key={i}>
+                  <Link to={"/beers/" + beer._id}>
+                    <div className="card m-2 p-2 text-center" style={{ width: "24rem", height: "18rem" }}>
+                      <div className="card-body">
+                        <img
+                          src={beer.image_url}
+                          style={{ height: "6rem" }}
+                          alt={"image of" + beer.name}
+                        />
+                        <h5 className="card-title text-truncate mt-2">{beer.name}</h5>
+                        <h6 className="card-subtitle mb-3 text-muted">
+                          <em>{beer.tagline}</em>
+                        </h6>
+                        <p className="card-text">
+                          Created by: {beer.contributed_by}
+                        </p>
+                      </div>
                     </div>
-                  </div>
-                </Link>
-              </div>
-            );
-          })}
-      </div>
+                  </Link>
+                </div>
+              );
+            })}
+        </div>
     </>
   );
 }
