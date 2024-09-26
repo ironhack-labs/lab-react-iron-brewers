83be06132c4311456b05a3f63f2d12f9144ac73f
enriquepaez <enrique.paezpozo@gmail.com>
2024-09-26 09:53:30 +0200

    Lab finished

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,26 +1,25 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Search from "../components/Search";
-import beersJSON from "./../assets/beers.json";
 import axios from "axios";
-
 
 
 function AllBeersPage() {
   // Mock initial state, to be replaced by data from the API. Once you retrieve the list of beers from the Beers API store it in this state variable.
   const [beers, setBeers] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("")
 
   // TASKS:
   // 1. Set up an effect hook to make a request to the Beers API and get a list with all the beers.
   useEffect(() => {
     getData()
-  }, [])
+  }, [searchQuery])
 
   const getData = async () => {
 
     try {
-        // 2. Use axios to make a HTTP request.
-      const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/beers`)
+      // 2. Use axios to make a HTTP request.
+      const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/beers/search?q=${searchQuery}`)
       console.log(response)
 
       // 3. Use the response data from the Beers API to update the state variable.
@@ -35,11 +34,13 @@ function AllBeersPage() {
   // The logic and the structure for the page showing the list of beers. You can leave this as it is for now.
   return (
     <>
-      <Search />
+      <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
 
       <div className="d-inline-flex flex-wrap justify-content-center align-items-center w-100 p-4">
           {beers &&
-            beers.map((beer, i) => {
+            beers
+            //.filter(beer => beer.name.toLowerCase().includes(searchQuery.toLowerCase()))
+            .map((beer, i) => {
               return (
                 <div key={i}>
                   <Link to={"/beers/" + beer._id}>
