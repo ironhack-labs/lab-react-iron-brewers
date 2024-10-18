9d8067ee5cdc2f3dc26922f877d25e5e6a094e75
YOUR NAME <94797132+PARAMET7@users.noreply.github.com>
2024-10-18 11:31:00 +0200

    done

--- src/pages/BeerDetailsPage.jsx
+++ src/pages/BeerDetailsPage.jsx
@@ -1,16 +1,27 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import beersJSON from "./../assets/beers.json";
-
+import axios from "axios";
 
 function BeerDetailsPage() {
   // Mock initial state, to be replaced by data from the Beers API. Store the beer info retrieved from the Beers API in this state variable.
-  const [beer, setBeer] = useState(beersJSON[0]);
-
+  const [beer, setBeer] = useState(null);
+  const {beerId} =useParams()
   // React Router hook for navigation. We use it for the back button. You can leave this as it is.
   const navigate = useNavigate();
 
-
+  useEffect(() => {
+    console.log("Beer ID from URL:", beerId); // Debugging
+    const fetchBeer = async () => {
+      try {
+        const response = await axios.get(`https://ih-beers-api2.herokuapp.com/beers/${beerId}`);
+        setBeer(response.data);
+      } catch (error) {
+        console.error("Error fetching beer details:", error);
+      }
+    };
+    fetchBeer();
+  }, [beerId]);
 
   // TASKS:
   // 1. Get the beer ID from the URL, using the useParams hook.
@@ -23,7 +34,7 @@ function BeerDetailsPage() {
   // Structure and the content of the page showing the beer details. You can leave this as it is:
   return (
     <div className="d-inline-flex flex-column justify-content-center align-items-center w-100 p-4">
-      {beer && (
+      {beer ? (
         <>
           <img
             src={beer.image_url}
@@ -46,6 +57,8 @@ function BeerDetailsPage() {
             Back
           </button>
         </>
+      ): (
+        <p>Loading...</p>
       )}
     </div>
   );
