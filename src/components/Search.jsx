3de85eac1b36d4a0b13024e32274e4b16c8f8f57
fiona-eh <fionahulse@mac.com>
2024-10-30 16:27:24 +0100

    initial solution

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,14 @@
+import { useState } from "react";
+
 function Search() {
+
+  const [items, setItems] = useState([]); 
+  const [query, setQuery] = useState("");
+
+  const filteredItems = items.filter(item => {
+    item.toLowerCase().includes(query.toLowerCase())
+  } )
+
   return (
     <div className="d-inline-flex justify-content-center align-items-center w-100 p-4">
       <div className="input-group mb-2 w-50">
@@ -7,7 +17,9 @@ function Search() {
             Search
           </span>
         </div>
-        <input
+        <input 
+          value={query}
+          onChange={e => setQuery(e.target.value)}
           type="text"
           className="form-control search-bar"
         />
