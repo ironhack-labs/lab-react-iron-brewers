42660ab57bc2cade8b3194d10d7f034c35f4bac6
Luke Farrell <lukefarrell2012@gmail.com>
2024-11-16 14:18:07 +0000

    lab complete

--- src/pages/BeerDetailsPage.jsx
+++ src/pages/BeerDetailsPage.jsx
@@ -1,33 +1,46 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import beersJSON from "./../assets/beers.json";
-
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom"; // Add useParams
+import fetchWrapper from "../utils/fetchWrapper";
 
 function BeerDetailsPage() {
-  // Mock initial state, to be replaced by data from the Beers API. Store the beer info retrieved from the Beers API in this state variable.
-  const [beer, setBeer] = useState(beersJSON[0]);
-
-  // React Router hook for navigation. We use it for the back button. You can leave this as it is.
+  const [beer, setBeer] = useState(null);
+  const [error, setError] = useState(null);
+  
   const navigate = useNavigate();
+  const { beerId } = useParams();
+
+  useEffect(() => {
+    loadBeerDetails();
+  }, [beerId]);
+
+  const loadBeerDetails = async () => {
+    const { data, error } = await fetchWrapper.get(`/beers/${beerId}`);
+    
+    if (error) {
+      setError(error);
+      setBeer(null);
+    } else {
+      setBeer(data);
+      setError(null);
+    }
+  };
 
 
+  if (error) {
+    return (
+      <div className="alert alert-danger m-3">
+        Error loading beer details: {error}
+      </div>
+    );
+  }
 
-  // TASKS:
-  // 1. Get the beer ID from the URL, using the useParams hook.
-  // 2. Set up an effect hook to make a request for the beer info from the Beers API.
-  // 3. Use axios to make a HTTP request.
-  // 4. Use the response data from the Beers API to update the state variable.
-
-
-
-  // Structure and the content of the page showing the beer details. You can leave this as it is:
   return (
     <div className="d-inline-flex flex-column justify-content-center align-items-center w-100 p-4">
       {beer && (
         <>
           <img
             src={beer.image_url}
-            alt="Beer Image"
+            alt={`${beer.name} beer`}
             height="300px"
             width="auto"
           />
@@ -39,9 +52,7 @@ function BeerDetailsPage() {
 
           <button
             className="btn btn-primary"
-            onClick={() => {
-              navigate(-1);
-            }}
+            onClick={() => navigate(-1)}
           >
             Back
           </button>
