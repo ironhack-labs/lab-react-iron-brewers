5264170e781f47a4e5e48263857f2e7ea560ad73
Blancadum <blancadum@gmail.com>
2025-03-24 20:45:50 +0100

    done

--- src/pages/BeerDetailsPage.jsx
+++ src/pages/BeerDetailsPage.jsx
@@ -1,54 +1,54 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import beersJSON from "./../assets/beers.json";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Search from "../components/Search";
+import axios from "axios";
 
+function AllBeersPage() {
+  const [beers, setBeers] = useState([]);
 
-function BeerDetailsPage() {
-  // Mock initial state, to be replaced by data from the Beers API. Store the beer info retrieved from the Beers API in this state variable.
-  const [beer, setBeer] = useState(beersJSON[0]);
+  // Obtener todas las cervezas al cargar la página
+  useEffect(() => {
+    axios
+      .get("https://ih-beers-api2.herokuapp.com/beers")
+      .then((response) => {
+        setBeers(response.data);
+      })
+      .catch((error) => {
+        console.error("Error al obtener cervezas:", error);
+      });
+  }, []);
 
-  // React Router hook for navigation. We use it for the back button. You can leave this as it is.
-  const navigate = useNavigate();
-
-
-
-  // TASKS:
-  // 1. Get the beer ID from the URL, using the useParams hook.
-  // 2. Set up an effect hook to make a request for the beer info from the Beers API.
-  // 3. Use axios to make a HTTP request.
-  // 4. Use the response data from the Beers API to update the state variable.
-
-
-
-  // Structure and the content of the page showing the beer details. You can leave this as it is:
   return (
-    <div className="d-inline-flex flex-column justify-content-center align-items-center w-100 p-4">
-      {beer && (
-        <>
-          <img
-            src={beer.image_url}
-            alt="Beer Image"
-            height="300px"
-            width="auto"
-          />
-          <h3>{beer.name}</h3>
-          <p>{beer.tagline}</p>
-          <p>Attenuation level: {beer.attenuation_level}</p>
-          <p>Description: {beer.description}</p>
-          <p>Created by: {beer.contributed_by}</p>
+    <>
+      <h1 className="text-center my-4">Todas las Cervezas</h1>
+      <Search />
 
-          <button
-            className="btn btn-primary"
-            onClick={() => {
-              navigate(-1);
-            }}
-          >
-            Back
-          </button>
-        </>
-      )}
-    </div>
+      <div className="d-inline-flex flex-wrap justify-content-center align-items-center w-100 p-4">
+        {beers.map((beer) => (
+          <div key={beer._id}>
+            <Link to={"/beers/" + beer._id}>
+              <div className="card m-2 p-2 text-center" style={{ width: "24rem", height: "18rem" }}>
+                <div className="card-body">
+                  <img
+                    src={beer.image_url}
+                    alt={"image of " + beer.name}
+                    style={{ height: "6rem" }}
+                  />
+                  <h5 className="card-title text-truncate mt-2">{beer.name}</h5>
+                  <h6 className="card-subtitle mb-3 text-muted">
+                    <em>{beer.tagline}</em>
+                  </h6>
+                  <p className="card-text">
+                    Created by: {beer.contributed_by}
+                  </p>
+                </div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
-export default BeerDetailsPage;
+export default AllBeersPage;
