b03a91658f0ff933d575d628785643aa547c9155
Tayyba Kanwal <taybarashid215@gmail.com>
2025-04-30 19:41:38 +0200

    lab-done

--- src/pages/AddBeerPage.jsx
+++ src/pages/AddBeerPage.jsx
@@ -1,4 +1,8 @@
+
 import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 
 function AddBeerPage() {
   // State variables to store the values of the form inputs. You can leave these as they are.
@@ -20,21 +24,56 @@ function AddBeerPage() {
   const handleBrewersTips = (e) => setBrewersTips(e.target.value);
   const handleAttenuationLevel = (e) => setAttenuationLevel(e.target.value);
   const handleContributedBy = (e) => setContributedBy(e.target.value);
-
+  const navigate = useNavigate()
 
 
   // TASK:
   // 1. Create a function to handle the form submission and send the form data to the Beers API to create a new beer.
   // 2. Use axios to make a POST request to the Beers API.
   // 3. Once the beer is created, navigate the user to the page showing the list of all beers.
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const newBeer = {
+      "name": name,
+      "tagline": tagline,
+      "description": description,
+      "imageUrl": imageUrl,
+      "firstBrewed": firstBrewed,
+      "brewersTips": brewersTips,
+      "attenuationLevel": attenuationLevel,
+      "contributedBy": contributedBy,    
+  }
+ 
+ 
+ 
+  setName("");
+    setTagline("");
+    setDescription("");
+    setImageUrl("");
+    setFirstBrewed("");
+    setBrewersTips("");
+    setAttenuationLevel("");
+    setContributedBy("");
 
+
+  axios.post("https://ih-beers-api2.herokuapp.com/beers/new",newBeer)
+  .then(()=>{
+    navigate("/beers")
+  })
+  .catch(e => console.log("Error creating a new beer...", e));
+     }
+    
+  
+  
 
 
   // Structure and the content of the page showing the form for adding a new beer. You can leave this as it is.
   return (
     <>
       <div className="d-inline-flex flex-column w-100 p-4">
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>Name</label>
           <input
             className="form-control mb-4"
@@ -130,3 +169,30 @@ function AddBeerPage() {
 }
 
 export default AddBeerPage;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
