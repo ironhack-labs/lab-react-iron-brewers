4f19b671fae506524cfbd00b4c3ee328428e0b75
oscarigu <38469752+Oscarigu@users.noreply.github.com>
2025-10-02 01:29:56 +0200

    solve lab

--- src/pages/BeerDetailsPage.jsx
+++ src/pages/BeerDetailsPage.jsx
@@ -1,24 +1,36 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState , useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import beersJSON from "./../assets/beers.json";
-
+import axios from "axios";
 
 function BeerDetailsPage() {
   // Mock initial state, to be replaced by data from the Beers API. Store the beer info retrieved from the Beers API in this state variable.
   const [beer, setBeer] = useState(beersJSON[0]);
-
+  const [loading, setLoading] = useState(true);
   // React Router hook for navigation. We use it for the back button. You can leave this as it is.
   const navigate = useNavigate();
-
-
+  const {beerId} = useParams();
+  const url = `https://beers-api.edu.ironhack.com/beers/${beerId}`;
 
   // TASKS:
   // 1. Get the beer ID from the URL, using the useParams hook.
   // 2. Set up an effect hook to make a request for the beer info from the Beers API.
   // 3. Use axios to make a HTTP request.
   // 4. Use the response data from the Beers API to update the state variable.
+  useEffect(()=>{
+        axios
+      .get(url)
+      .then((response) => {
+        setBeer(response.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error("Error fetching beer details:", err);
+        setLoading(false);
+      });
 
-
+  },[beerId])
+ if (loading) return <p className="p-4">Loading beer details...</p>;
 
   // Structure and the content of the page showing the beer details. You can leave this as it is:
   return (
